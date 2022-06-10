ff325a1afc22b11e3b20df174b1ec029d8a70508
Jmos-Mbugua <johnmbugua849@gmail.com>
2022-06-10 11:28:16 -0700

    define if a character is a number

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isdigit - input
+ *
+ * Description: define if a character is a number
+ * @c: character
+ * Return: 1 yes or no
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
+
